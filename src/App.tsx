17451f4ac8ee11fd9e02323476c6111acdd51c6d
James Serengia <jamesserengia@gmail.com>
2024-03-28 07:03:46 +0300

    Add simple add goal implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  const addGoalHandler = () => {
+    const newGoal = {
+      title: "My first Goal",
+      description: "First goal description",
+      id: Math.floor(Math.random() * 100),
+    };
+    // Add goal
+    setGoals((prevValues) => {
+      return [newGoal, ...prevValues];
+    });
+  };
   return (
     <main>
-      <CourseGoal title="Learn react + Typescript">
-        <p>Learn React - Typescript from the scratch</p>
-      </CourseGoal>
+      <Header image={{ src: "/goals.jpg", alt: "Goal image" }}>
+        <h2>Your Course Goals </h2>
+      </Header>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      {goals.map((g) => (
+        <CourseGoal title={g.title}>
+          <p>{g.description}</p>
+        </CourseGoal>
+      ))}
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -2,7 +2,6 @@
 // type CourseGoalProps = PropsWithChildren<{title: string}>
 
 import { type ReactNode } from "react";
-import Header from "./Header";
 
 type CourseGoalProps = {
   title: string;
@@ -12,9 +11,6 @@ type CourseGoalProps = {
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
-      <Header image={{ src: "/goals.jpg", alt: "Goal image" }}>
-        <h2>Your Course Goals </h2>
-      </Header>
       <div>
         <h2>{title}</h2>
         {children}

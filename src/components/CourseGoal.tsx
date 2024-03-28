dc9b73c224b6141224175044f4b80e3b6ab4b6c9
James Serengia <jamesserengia@gmail.com>
2024-03-28 06:37:18 +0300

    Add header component

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -2,6 +2,7 @@
 // type CourseGoalProps = PropsWithChildren<{title: string}>
 
 import { type ReactNode } from "react";
+import Header from "./Header";
 
 type CourseGoalProps = {
   title: string;
@@ -11,6 +12,9 @@ type CourseGoalProps = {
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
+      <Header image={{ src: "/goals.jpg", alt: "Goal image" }}>
+        <h2>Your Course Goals </h2>
+      </Header>
       <div>
         <h2>{title}</h2>
         {children}
